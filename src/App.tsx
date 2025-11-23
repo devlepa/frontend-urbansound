05958cf9fd4d19b233936c60872a85bc8545e544
devlepa <juanestebanleonparra2@gmail.com>
2025-11-23 17:04:19 -0500

    force rebuild amplify

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div style={{ maxWidth: 600, margin: "40px auto", textAlign: "center" }}>
-      <h1>UrbanSound Classifier</h1>
+      <h1>UrbanSound Classifier 🔊</h1>
       <Recorder />
     </div>
   );
