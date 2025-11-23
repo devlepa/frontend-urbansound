cd1a070b773a3de3497a2a56b35c4df42ab99379
devlepa <juanestebanleonparra2@gmail.com>
2025-11-23 18:05:26 -0500

    test frontend render

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import Recorder from "./components/Recorder";
-import "./App.css";
-
 function App() {
   return (
     <div style={{ maxWidth: 600, margin: "40px auto", textAlign: "center" }}>
-      <h1>UrbanSound Classifier 🔊</h1>
-      <Recorder />
+      <h1>UrbanSound Classifier 🎧</h1>
+      <p>Test rendering</p>
     </div>
   );
 }
